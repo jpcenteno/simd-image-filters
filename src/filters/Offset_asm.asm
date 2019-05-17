d9a0b40b7fe6b19603fffeb6abde6b9a21e086d4
Joaquin Romera <joaquin.romera@ibm.com>
2019-05-17 11:21:58 -0300

    Listo comments en Offset

--- src/filters/Offset_asm.asm
+++ src/filters/Offset_asm.asm
@@ -25,81 +25,76 @@ Offset_asm:
   
   xor r10, r10
   xor r11, r11
-  shl rdx, 2
-  sub ecx, 16
-
-  xor r8, r8
-  xor r9, r9
+  shl rdx, 2                              ; rdx <-- width en bytes
+  sub ecx, 16                             ; se consideran el offset+8 y el borde de 8 pixeles
 
   call paintBordersASM
 
-  times 8 add rsi, rdx
+  times 8 add rsi, rdx                    ; se avanzan los punteros hasta luego del borde
   times 8 add rdi, rdx
 
-  mov r9, rdx
-  sub r9, 64
+  mov r9, rdx                             ; r9 <-- width en bytes
+  sub r9, 64                              ; offset+8 y 8 pixeles del borde
 
-  mov rax, rdi
-  add rax, 32
+  mov rax, rdi                            ; rax <-- puntero a fila actual en img src
+  add rax, 32                             ; 8 pixeles * 4 bytes = 32, los primeros 8 pixeles del borde
+  mov r12, rax                            ; r12 <-- puntero a primer pixel a procesar en src
   
-  mov r8, rsi
-  add r8, 32
-
-  xor r12, r12
-  xor r13, r13
-
-  mov r12, rax
-  mov r13, r8
+  mov r8, rsi                             ; r8 <-- puntero a fila actual en img dst
+  add r8, 32                              ; 8 pixeles * 4 bytes = 32, los primeros 8 pixeles del borde
+  mov r13, r8                             ; r13 <-- puntero a primer pixel a guardar en dst
 
 .forEachRow:
   cmp r10d, ecx
   je .end
   xor r11, r11
+
   .forEachColumn:
     cmp r11, r9
     je .nextRow
-    movdqu xmm0, [rax + r11]
+    movdqu xmm0, [rax + r11]              ; xmm0 <-- [ A3 R3 G3 B3 | A2 R2 G2 B2 | A1 R1 G1 B1 | A0 R0 G0 B0 ]
 
     ; src[i+8][j+8].r
-    mov r12, rax
-    add r12, r11
-    add r12, 32
-    movdqu xmm1, [r12 + rdx * 8]
-    pand xmm1, xmm11
+    mov r12, rax                          ; r12 <-- puntero a fila actual
+    add r12, r11                          ; r12 <-- puntero a fila actual y pixel actual
+    add r12, 32                           ; r12 <-- puntero a fila actual y pixel actual + 8
+    movdqu xmm1, [r12 + rdx * 8]          ; xmm1 <-- [ idem P3 | idem P2 | idem P1 | A0[+8][+8] R0[+8][+8] G0[+8][+8] B0[+8][+8] ]
+    pand xmm1, xmm11                      ; xmm1 <-- [ idem P3 | idem P2 | idem P1 | 0 R0[+8][+8] 0 0 ]
 
     ; src[i][j+8].g
-    mov r12, rax
-    add r12, r11
-    add r12, 32
-    movdqu xmm2, [r12]
-    pand xmm2, xmm12
+    mov r12, rax                          ; r12 <-- puntero a fila actual
+    add r12, r11                          ; r12 <-- puntero a fila actual y pixel actual
+    add r12, 32                           ; r12 <-- puntero a fila actual y pixel actual + 8
+    movdqu xmm2, [r12]                    ; xmm2 <-- [ idem P3 | idem P2 | idem P1 | A0[+0][+8] R0[+0][+8] G0[+0][+8] B0[+0][+8] ]
+    pand xmm2, xmm12                      ; xmm2 <-- [ idem P3 | idem P2 | idem P1 | 0 0 G0[+0][+8] 0 ]
 
     ; src[i+8][j].b
-    mov r12, rax
-    add r12, r11
-    movdqu xmm3, [r12 + rdx * 8]
-    pand xmm3, xmm13
+    mov r12, rax                          ; r12 <-- puntero a fila actual
+    add r12, r11                          ; r12 <-- puntero a fila actual y pixel actual
+    movdqu xmm3, [r12 + rdx * 8]          ; xmm3 <-- [ idem P3 | idem P2 | idem P1 | A0[+8][+0] R0[+8][+0] G0[+8][+0] B0[+8][+0] ]
+    pand xmm3, xmm13                      ; xmm3 <-- [ idem P3 | idem P2 | idem P1 | 0 0 0 B0[+8][+0] ]
 
-    pxor xmm0, xmm0
-    paddq xmm0, xmm1
-    paddq xmm0, xmm2
-    paddq xmm0, xmm3
-    paddq xmm0, xmm10
+    pxor xmm0, xmm0                       ; xmm0 <-- [ 0 | 0 | 0 | 0 ]
+    por xmm0, xmm1                        ; xmm0 <-- [ idem P3 | idem P2 | idem P1 | 0 R0[+8][+8] 0 0 ]
+    por xmm0, xmm2                        ; xmm0 <-- [ idem P3 | idem P2 | idem P1 | 0 R0[+8][+8] G0[+0][+8] 0 ]
+    por xmm0, xmm3                        ; xmm0 <-- [ idem P3 | idem P2 | idem P1 | 0 R0[+8][+8] G0[+0][+8] B0[+8][+0] ]
+    por xmm0, xmm10                       ; xmm0 <-- [ idem P3 | idem P2 | idem P1 | 0xFF R0[+8][+8] G0[+0][+8] B0[+8][+0] ]
 
-    movdqu [r8 + r11], xmm0
-    add r11, 16
+    movdqu [r8 + r11], xmm0               ; guardo 4 pixeles procesados
+    add r11, 16                           ; avanzo a los 4 siguientes
     jmp .forEachColumn
+
   .nextRow:
-    add rdi, rdx
-    add rsi, rdx
+    add rdi, rdx                          ; avanzo a siguiente fila dst
+    add rsi, rdx                          ; avanzo a siguiente fila src
 
     mov rax, rdi
-    add rax, 32
+    add rax, 32                           ; rax <-- siguiente fila dst, primer pixel a procesar luego del borde
 
     mov r8, rsi
-    add r8, 32
+    add r8, 32                            ; r8 <-- siguiente fila src, primer pixel a guardar luego del borde
 
-    inc r10d
+    inc r10d                              ; siguiente fila
     jmp .forEachRow
 
 .end:
@@ -108,7 +103,14 @@ Offset_asm:
   pop rbp
   ret
 
+
 paintBordersASM:
+  ; rdi <-- uint8_t *src
+  ; rsi <-- uint8_t *dst
+  ; rdx <-- int width en bytes
+  ; ecx <-- int height menos border y offset
+  ; r8d  <-- int src_row_size
+  ; r9d  <-- int dst_row_size
   push rbp
   mov rbp, rsp
   push rcx
@@ -124,14 +126,16 @@ paintBordersASM:
   cmp r8, 0
   je .continueWithMiddle
   xor r9, r9
+
   .nextTopRowColumn:
     cmp r9, rdx
     je .nextTopRow
-    movdqu [rsi + r9], xmm10
-    add r9, 16
+    movdqu [rsi + r9], xmm10            ; guardo en src, xmm10 = [ idem P3 | idem P2 | idem P1 | 0xFF 0 0 0 ]
+    add r9, 16                          ; guardo de a 4
     jmp .nextTopRowColumn
+
   .nextTopRow:
-    add rsi, rdx
+    add rsi, rdx                        ; prox fila
     dec r8
     jmp .topRows
 
@@ -143,12 +147,12 @@ paintBordersASM:
 .middleRows:
   cmp r8, rcx
   je .continueWithBottom
-  movdqu [rsi], xmm10
-  movdqu [rsi + 16], xmm10
-  movdqu [rsi + r9], xmm10
+  movdqu [rsi], xmm10                   ; guardo en src, xmm10 = [ idem P3 | idem P2 | idem P1 | 0xFF 0 0 0 ]
+  movdqu [rsi + 16], xmm10              ; idem para completar el primer borde vertical
+  movdqu [rsi + r9], xmm10              ; idem para el segundo
   movdqu [rsi + r9 + 16], xmm10
   add rsi, rdx
-  inc r8
+  inc r8                                ; prox fila
   jmp .middleRows
 
 .continueWithBottom:
@@ -158,14 +162,16 @@ paintBordersASM:
   cmp r8, 0
   je .end
   xor r9, r9
+
   .nextBottomRowColumn:
     cmp r9, rdx
     je .nextBottomRow
-    movdqu [rsi + r9], xmm10
-    add r9, 16
+    movdqu [rsi + r9], xmm10            ; guardo en src, xmm10 = [ idem P3 | idem P2 | idem P1 | 0xFF 0 0 0 ]
+    add r9, 16                          ; guardo de a 4
     jmp .nextBottomRowColumn
+
   .nextBottomRow:
-    add rsi, rdx
+    add rsi, rdx                        ; prox fila
     dec r8
     jmp .bottomRows
 
